a005c93c74f116d4eb0ac0571efd0e60ff377161
ksh-fthr <ksh.fthr0806@gmail.com>
2018-10-14 21:10:54 +0900

    親子間でデータ(オブジェクト)を渡す処理を試した。

---
+++ src/components/UserList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>This page is user list.</h1>
+    <!-- <div class="loading" v-if="loading">Loading Now...</div> -->
+    <!-- <div class="error" v-if="error"> -->
+      <!-- {{ error }} -->
+    <!-- </div> -->
+    <div v-for="user in users" :key="user.id">
+      <h2>{{ user.name }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  props: ['users']
+}
+/*
+var List = {
+  data: function () {
+    return {
+      loading: false,
+      users: function () { return [] },
+      error: null
+    }
+  },
+
+  created: function () {
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': 'fetchData'
+  },
+
+  methods: {
+    fetchData: function () {
+      this.loading = true
+      getUsers(function (err, users) {
+        this.loading = false
+        if (err) {
+          this.error = err.toString()
+        } else {
+          this.users = users
+        }
+      }).bind(this)
+    }
+  }
+}
+
+var getUsers = function (callback) {
+  setTimeout(function () {
+    callback(null, [
+      {
+        id: 1,
+        name: 'hogehoge'
+      },
+      {
+        id: 2,
+        name: 'barbar'
+      }
+    ])
+  }, 1000 * 1)
+}
+*/
+</script>
---
+++ src/views/Users.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="users">
+    <UserList :users="list" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserList from '@/components/UserList.vue'
+
+export default {
+  name: 'users',
+  data: function () {
+    return {
+      list: [
+        {
+          id: 1,
+          name: 'hogehoge'
+        },
+        {
+          id: 2,
+          name: 'barbar'
+        }
+      ]
+    }
+  },
+  components: {
+    UserList
+  }
+}
+
+</script>
