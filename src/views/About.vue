f1626036c1e74a38087325787433950ba3d50fd5
ksh-fthr <ksh-fthr@ksh-fthrnoMacBook.local>
2018-11-17 12:10:13 +0900

    UIコンポーネント「Buefy」を導入した

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Collapse/>
   </div>
 </template>
+
+<script>
+import Collapse from '@/components/Collapse'
+
+export default {
+  name: 'about',
+  components: {
+    Collapse
+  }
+}
+</script>
