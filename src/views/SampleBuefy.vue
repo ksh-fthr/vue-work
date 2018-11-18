4428b231244269975b5ff58a9de80ecf7b600f5b
ksh-fthr <ksh-fthr@ksh-fthrnoMacBook.local>
2018-11-18 19:58:54 +0900

    DatePicker と Modal のサンプルコード実装

---
+++ src/views/SampleBuefy.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sample-buefy">
+    <BuefyParts/>
+  </div>
+</template>
+
+<script>
+import BuefyParts from '@/components/BuefyParts'
+
+export default {
+  name: 'sample-buefy',
+  components: {
+    BuefyParts
+  }
+}
+</script>
