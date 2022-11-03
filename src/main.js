01688be60258bda77701adfcc1f63f1b157152c8
ksh-fthr <ksh.fthr0806@gmail.com>
2022-11-03 19:04:13 +0900

    wip, v3 に移行中.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-Vue.use(Buefy)
+//import Buefy from 'buefy'
+//import 'buefy/dist/buefy.css'
+//Vue.use(Buefy)
 
 import App from './App.vue'
 import router from './router'
